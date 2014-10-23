2f5d5259a0d8e68aa0419f40225234afe91501af
Dan Truong <truong.dan@gmail.com>
2014-10-22 22:17:41 -0400

    finished module 5

--- MoviesApp/app/controllers/movies_controller.rb
+++ MoviesApp/app/controllers/movies_controller.rb
@@ -1,12 +1,16 @@
 class MoviesController < ApplicationController
 	def new 
+		@movie = Movie.new
 	end
 
 	def create
 		@movie = Movie.new(movie_params)
 
-		@movie.save
-		redirect_to @movie
+		if @movie.save
+			redirect_to @movie
+		else
+			render 'new'
+		end
 	end
 
 	def show
@@ -16,7 +20,29 @@ class MoviesController < ApplicationController
 	def index
 		@movies = Movie.all
 	end
-	
+
+	def edit
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+
+		if @movie.update(movie_params)
+			redirect_to @movie
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+
+		redirect_to movies_path
+		
+	end
+
 	private
 	def movie_params
 		params.require(:movie).permit(:title, :description, :year_released)
